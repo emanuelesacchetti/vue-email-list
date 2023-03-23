3ee7647cdc9300d3f937b4417e20b3a349d55dfa
emanuele sacchetti <manusacco92@gmail.com>
2023-03-23 14:29:15 +0100

    compito base completato: in mounted creato un ciclo che generi per 10 volte email random tramite API boolean e che pushi i risultati in un array. in HTML creata una lista di elementi dinamicamente e inseriti tutti gli elementi dell'array

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const { createApp } = Vue
+
+createApp({
+data() {
+    return {
+    emailList: [],
+    pippo: ''
+    }
+},
+mounted(){
+    for(i = 0; i < 10; i++){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response)=>{
+            this.emailList.push(response.data.response);
+        });
+    }
+    
+
+}
+}).mount('#app')
